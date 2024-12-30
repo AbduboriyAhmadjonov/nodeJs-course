f2ff3ea7b6f37ad0a1d56ca256287052b4cbc945
Ahmadjonov Abduboriy <ahmadjonovabduboriy3@gmail.com>
2024-12-30 22:18:49 +0500

    Logging products which created by admin

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const path = require('path');
-
 const express = require('express');
-const adminData = require('./routes/admin');
+const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 // If it cannot find those, it will show this
